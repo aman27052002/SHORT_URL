2dd69533a72972a5565aaf06eb549f4c6e96d463
Aman Kothari <kothariaman2705@gmail.com>
2025-03-15 20:27:22 +0530

    Updated add login signup page and add authentication

--- index.js
+++ index.js
@@ -13,16 +13,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 
 ConnectToDB()
--- service/auth.js
+++ service/auth.js
@@ -4,7 +4,12 @@ function setUser(user){
   return jwt.sign({name,email,_id},process.env.JWT_SECRET)
 }
 function getUser(token){
- return jwt.verify(token,process.env.JWT_SECRET)
+  if(!token)return null
+  try {
+    return jwt.verify(token,process.env.JWT_SECRET)
+  } catch (error) {
+    return null
+  }
 }
 
 module.exports = {setUser,getUser}
